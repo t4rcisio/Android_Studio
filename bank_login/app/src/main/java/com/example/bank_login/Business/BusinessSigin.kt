5bb27eecd883d280ac2ee572ef890659b647b2b1
t4rcisio <tarcisiobatistaprates@gmail.com>
2021-09-23 20:09:05 -0300

    password event

--- bank_login/app/src/main/java/com/example/bank_login/Business/BusinessSigin.kt
+++ bank_login/app/src/main/java/com/example/bank_login/Business/BusinessSigin.kt
@@ -1,28 +1,56 @@
 package com.example.bank_login.Business
 
+import android.content.Context
+import android.graphics.Typeface
+import android.os.Build
 import android.text.InputType
-import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
-import com.example.bank_login.ui.LoginActivity
-import kotlinx.android.synthetic.main.activity_login.*
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.toColor
+import com.example.bank_login.R
 
-class BusinessSigin {
+
+class BusinessSigin(var context: Context) {
 
 
-    fun changePasswordVisibility(password: EditText, icon: ImageView, id_hidden : Int,  id_visible: Int){
+
+    fun changePasswordVisibility(password: EditText, icon: ImageView){
+
         if(password.inputType == 129){
-            println("Texto")
             password.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
             password.setSelection(password.length())
-
-            icon.setImageResource(id_visible)
+            password.setTypeface(Typeface.SANS_SERIF);
+            icon.setImageResource(R.drawable.ic_baseline_visibility_24)
         }else{
-            println("Senha")
             password.inputType = 129
             password.setSelection(password.length())
-            icon.setImageResource(id_hidden)
+            icon.setImageResource(R.drawable.ic_baseline_visibility_off_24)
+            password.setTypeface(Typeface.SANS_SERIF);
         }
+
+    }
+
+    fun emptyPassoword(password: EditText, passwordText: TextView, errorEmpty: TextView): Boolean{
+        return if(password.text.toString().any()){
+            password.background.setTint(ContextCompat.getColor(context,R.color.editTextDef))
+            passwordText.setTextColor(ContextCompat.getColor(context,R.color.text))
+            errorEmpty.setTextColor(ContextCompat.getColor(context,R.color.text))
+            errorEmpty.setText(R.string.empty)
+            true
+        }else{
+
+            password.background.setTint(ContextCompat.getColor(context, R.color.errorBox))
+            passwordText.setTextColor(ContextCompat.getColor(context,R.color.errorTex))
+            errorEmpty.setTextColor(ContextCompat.getColor(context,R.color.errorTex))
+            errorEmpty.setText(R.string.campo_obrigat_rio)
+            false
+        }
+    }
+
+    fun sigin(){
 
     }
 
--- bank_login/app/src/main/java/com/example/bank_login/ui/LoginActivity.kt
+++ bank_login/app/src/main/java/com/example/bank_login/ui/LoginActivity.kt
@@ -1,30 +1,60 @@
 package com.example.bank_login.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.bank_login.Business.BusinessSigin
 import com.example.bank_login.R
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
-    private var mBusinessSigin = BusinessSigin()
+    private lateinit var mBusinessSigin : BusinessSigin
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         if(supportActionBar != null)
             supportActionBar!!.hide()
-
+        mBusinessSigin = BusinessSigin(this)
         passwordVisibility.setOnClickListener(this)
+        siginbutton.setOnClickListener(this)
+        passwordChangeEvent()
+
     }
 
+    private fun passwordChangeEvent(){
+        password.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                println("before")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                mBusinessSigin.emptyPassoword(password,pswText,errorEmptyPassword)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
+    private fun onClick(){
+        if(mBusinessSigin.emptyPassoword(password,pswText,errorEmptyPassword)){
+           startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
 
 
     override fun onClick(v: View) {
         var id = v.id
         when(id){
-            R.id.passwordVisibility -> mBusinessSigin.changePasswordVisibility(password, passwordVisibility,R.drawable.ic_baseline_visibility_off_24,R.drawable.ic_baseline_visibility_24)
+            R.id.passwordVisibility -> mBusinessSigin.changePasswordVisibility(password, passwordVisibility)
+            R.id.siginbutton -> onClick()
         }
     }
+
+
 
 }
